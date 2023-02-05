b70ec97ee6f3fccf03563e651a1a3bf7d9a2e89d
tres-bien <denischudinov1212@gmail.com>
2023-02-05 18:56:30 +0200

    retake the homework

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -8,24 +8,25 @@ namespace Lesson_1
         static void Main(string[] args)
         {
             byte personAge = 24;
-            decimal minimumBankAccountLimit = -1000000000000;
-            decimal maxsimumBankAccountLimit = 1000000000000;
+            double bankAccount = -1000000000000; //наскільки знаю в фін.установах гроші зберігаються в decimal
             char letter = 'F';
             string message = "Hello World";
             bool answer = true;
             float humanTemperature = 36.6f;
-            long humanPopulation = 8014895000;
-            sbyte temperatureInUkraine = -2;
+            ulong humanPopulation = 8014895000;
+            float temperatureInUkraine = -2f; //термометром давно не користуюсь, але знаю що в гуглі температуру пишуть без дробей. Вважаю що це потрібно вказувати в тз
 
             Console.OutputEncoding = Encoding.Unicode;
             Console.WriteLine($"Вік людини: {personAge}\n" +
-                              $"Банківський рахунок з лімітом від: {minimumBankAccountLimit} до {maxsimumBankAccountLimit}\n" +
+                              $"Банківський рахунок: {bankAccount}\n" +
                               $"Літера: {letter}\n" +
                               $"SMS повідомлення: {message}\n" +
                               $"Дати оцінку твердженню: \"Сусід сказав, що людина може жити 1000 років\":.{answer}\n" +
                               $"Температура людини: {humanTemperature}\n" +
                               $"Кількість населення планети: {humanPopulation}\n" +
                               $"Температура навколишньго середовища в Україні: {temperatureInUkraine}");
+
+            Console.ReadLine();
         }
     }
 }
