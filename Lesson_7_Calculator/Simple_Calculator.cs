8d4170ad01fbeda4933ca000eda2022631655f96
tres-bien <denischudinov1212@gmail.com>
2023-02-23 17:03:53 +0200

    Lesson_7

--- Lesson_7_Calculator/Simple_Calculator.cs
+++ Lesson_7_Calculator/Simple_Calculator.cs
@@ -45,7 +45,7 @@ namespace Calculator
                     Console.WriteLine(operand1 / operand2);
                     break;
                 case '^':
-                    int square = operand1;
+                    int square = 1;
                     for (int i = 1; i < operand2; i++)
                     {
                         square *= operand1;
