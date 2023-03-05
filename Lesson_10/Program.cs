441f1f701ed534234fdd6b476ab4f83b5c87efad
tres-bien <denischudinov1212@gmail.com>
2023-03-05 19:48:56 +0200

    Lesson_10

---
+++ Lesson_10/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lesson_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"G:\myFolder\Hillel\Lesson_10\strings.txt");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            SentenseCounter(text);
+
+            Console.ReadLine();
+        }
+
+        static void SentenseCounter(string text)
+        {
+            char[] separators = new char[] { '.', '…', '!', '?' };
+            string[] sentenses = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            NameExtractor(sentenses);
+        }
+
+        static void NameExtractor(string[] sentenses)
+        {
+            StringBuilder names = new StringBuilder();
+            string[] words;
+            for (int i = 0; i < sentenses.Length; i++)
+            {
+                words = sentenses[i].Split(' ', ',', '.', '!', '?', ':', ';', '\t');
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (!sentenses[i].StartsWith(words[j]) && !sentenses[i].StartsWith(' ' + words[j]) && char.IsUpper(words[j], 0))
+                    {
+                        names.Append(words[j] + ' ');
+                    }
+                }
+            }
+            string[] names2 = names.ToString().Split(' ');
+            names = new StringBuilder(names2[0] + ' ');
+            for (int i = 0; i < names2.Length; i++)
+            {
+                for (int j = 0; j < names2.Length; j++)
+                {
+                    if (!names.ToString().Contains(names2[j]))
+                    {
+                        names.Append(names2[j] + ' ');
+                    }
+                }
+            }
+            string[] uniqueNamse = names.ToString().Split(' ');
+            Print(sentenses.Length, uniqueNamse);
+        }
+
+        static void Print(int sentenseCount, string[] uniqueNames)
+        {
+            Console.WriteLine($"Кількість речень: {sentenseCount}");
+            Console.WriteLine($"Кількість унікальних імен або прізвищ: {uniqueNames.Length}");
+            Console.Write("Унікальні імена та прізвища: ");
+
+            for (int i = 0; i < uniqueNames.Length; i++)
+            {
+                Console.Write(uniqueNames[i] + " ");
+            }
+        }
+    }
+}
